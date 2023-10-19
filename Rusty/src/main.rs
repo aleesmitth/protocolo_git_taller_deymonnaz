680a2e381da335677e6945a102377f45a9feaec6
mvalmaggia <mvalmaggia@DESKTOP-3K69428>
2023-10-19 20:24:09 -0300

    cat-file working

--- Rusty/src/main.rs
+++ Rusty/src/main.rs
@@ -1,15 +1,22 @@
-use std::{fs, error::Error, io::Write};
+use std::{fs, error::Error, io::Write, io::Read};
 extern crate crypto;
+extern crate libflate;
+
 use crypto::sha1::Sha1;
 use crypto::digest::Digest;
+use libflate::zlib::{Encoder, Decoder};
+use std::str;
 
 const GIT: &str = ".git";
 const OBJECT: &str = ".git/objects";
 const REFS: &str = ".git/refs";
 const R_HEADS: &str = ".git/refs/heads";
-const HEAD: &str = ".git/HEAD";
+const HEAD_FILE: &str = ".git/HEAD";
 const R_TAGS: &str = ".git/refs/tags";
 const DEFAULT_BRANCH_NAME: &str = "main";
+
+const TYPE: &str = "-t";
+const SIZE: &str = "-s";
 
 pub struct Init {
 
@@ -41,26 +48,56 @@ impl Head {
 	}
 }
 
-impl Command for Init {
-    fn execute(&self, head: &mut Head) -> Result<(), Box<dyn Error>>{
-        // let _dir = fs::create_dir(GIT)?;
-        // let _refs = fs::create_dir(REFS)?;
-        // let _obj = fs::create_dir(OBJECT)?;
-        // let _refs_heads = fs::create_dir(R_HEADS)?;
-        // head.add_branch("main");
-        // let mut head_file = fs::File::create(HEAD)?;
-        // head_file.write_all(b"ref: refs/heads/main")?;
-        // let _refs_tags = fs::create_dir(R_TAGS)?;
+pub struct CatFile;
 
-        // Ok(())
-        // usando create_dir_all() se puede evitar crear a todos uno por uno
+impl CatFile {
+    pub fn new () -> Self {
+        CatFile{}
+    }
+}
+
+impl Command for CatFile {
+    fn execute(&self, _head: &mut Head, args: Option<&[&str]>) -> Result<(), Box<dyn Error>> {
+        match args {
+            Some(args) => {
+
+                //let first_arg = args[1..];
+                let path = format!(".git/objects/{}/{}", &args[1][..2], &args[1][2..]);
+                let file = fs::File::open(path)?;
+
+                let mut decoder = Decoder::new(file)?;
+
+                let mut header = [0u8; 8];
+                decoder.read_exact(&mut header)?;
+                //let header_str = str::from_utf8(&header)?;
+
+                let header_str = str::from_utf8(&header)?;
+
+                // Extract the object type and size
+                let parts: Vec<&str> = header_str.trim_end().split(' ').collect();
+
+                match args[0] {
+                    TYPE => println!("{}", parts[0]),
+                    SIZE => println!("{}", parts[1]),
+                    _ => eprintln!(""),
+                }
+            }
+            None => eprintln!("")
+        }
+
+        Ok(())
+    }
+}
+
+impl Command for Init {
+    fn execute(&self, head: &mut Head, args: Option<&[&str]>) -> Result<(), Box<dyn Error>> {
         let _refs_heads = fs::create_dir_all(R_HEADS);
         let _refs_tags = fs::create_dir(R_TAGS)?;
         let _obj = fs::create_dir(OBJECT)?;
 
-        create_new_branch(DEFAULT_BRANCH_NAME)?;
+        create_new_branch(DEFAULT_BRANCH_NAME, head)?;
 
-        let mut head_file = fs::File::create(HEAD)?;
+        let mut head_file = fs::File::create(HEAD_FILE)?;
         head_file.write_all(b"ref: refs/heads/main")?;
         
         Ok(())    
@@ -70,7 +107,7 @@ impl Command for Init {
 }
 
 pub trait Command {
-    fn execute(&self, head: &mut Head) -> Result<(), Box<dyn Error>>;
+    fn execute(&self, _head: &mut Head, args: Option<&[&str]>) -> Result<(), Box<dyn Error>>;
 }
 
 fn generate_sha1_string(branch_name: &str) -> String {
@@ -79,7 +116,7 @@ fn generate_sha1_string(branch_name: &str) -> String {
     hasher.result_str()
 }
 
-fn create_new_branch(branch_name: &str) -> Result<(), Box<dyn Error>> { 
+fn create_new_branch(branch_name: &str, head: &mut Head) -> Result<(), Box<dyn Error>> { 
     let branch_path = format!("{}/{}", R_HEADS, branch_name);
 
     let mut branch_file = fs::File::create(&branch_path)?;
@@ -92,10 +129,16 @@ fn create_new_branch(branch_name: &str) -> Result<(), Box<dyn Error>> {
 
 fn main() {
     let mut head = Head::new();
-    let init = Init::new();
-    if let Err(error) = init.execute(&mut head){
+    // let init = Init::new();
+    // if let Err(error) = init.execute(&mut head, None){
+    //     eprintln!("{}", error);
+    //     return; 
+    // }
+    head.print_all();
+
+    let mut cat = CatFile::new();
+    if let Err(error) = cat.execute(&mut head, Some(&["-t", "000142551ee3ec5d88c405cc048e1d5460795102"])){
         eprintln!("{}", error);
         return; 
     }
-    head.print_all();
 }
