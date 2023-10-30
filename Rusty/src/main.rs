e168d6d9bc5604d98d29444d36bd36ee82418f75
mvalmaggia <mvalmaggia@DESKTOP-3K69428>
2023-10-29 22:53:41 -0300

    commit almost done

--- Rusty/src/main.rs
+++ Rusty/src/main.rs
@@ -132,6 +132,51 @@ impl Command for Rm {
     }
 }
 
+pub struct Commit;
+
+impl Commit {
+    fn new() -> Self {
+        Commit {}
+    }
+
+    fn generate_commit_content(&self, tree_hash: String, message: &str, branch_path: &str) -> Result<String, Box<dyn Error>> {
+        let head_commit = read_file_content(branch_path)?;
+        
+        let mut content = String::new();
+        content.push_str(&tree_hash);
+        content.push_str(&head_commit);
+        content.push_str(message);
+        Ok(content)
+    }
+}
+
+impl Command for Commit {
+    fn execute(&self, head: &mut Head, args: Option<&[&str]>) -> Result<String, Box<dyn Error>> {
+        let mut message;
+        match args {
+            Some(args) =>  message = args[1],
+            None => message = "",
+        }
+        let hash_obj = HashObject::new();
+        let tree_hash = hash_obj.execute(head, Some(&[WRITE_FLAG, TYPE_FLAG, "tree", INDEX_FILE]))?;
+        let head_file_content = read_file_content(HEAD_FILE)?;
+        let split_head_content: Vec<&str> = head_file_content.split(" ").collect();
+
+        if let Some(branch_path) = split_head_content.get(1) {
+            let full_branch_path = format!(".git/{}", branch_path);
+            let commit_content = self.generate_commit_content(tree_hash, message, &full_branch_path)?;
+
+            let commit_object_hash = HashObjectCreator::write_object_file(commit_content.clone(), ObjectType::Commit, commit_content.as_bytes().len() as u64)?;
+
+            let mut branch_file = fs::File::create(full_branch_path)?;
+            branch_file.write_all(commit_object_hash.as_bytes())?;
+        }
+
+        Ok(String::new())
+    }
+}
+
+
 impl Head {	
 	pub fn new() -> Self {
 		Head { branches: Vec::new() }
@@ -180,8 +225,6 @@ impl Command for CatFile {
     fn execute(&self, _head: &mut Head, args: Option<&[&str]>) -> Result<String, Box<dyn Error>> {
         match args {
             Some(args) => {
-
-                //let first_arg = args[1..];
                 let path = format!(".git/objects/{}/{}", &args[1][..2], &args[1][2..]);
                 let file = fs::File::open(path)?;
 
@@ -189,7 +232,6 @@ impl Command for CatFile {
 
                 let mut header = [0u8; 8];
                 decoder.read_exact(&mut header)?;
-                //let header_str = str::from_utf8(&header)?;
 
                 let header_str = str::from_utf8(&header)?;
 
@@ -221,7 +263,7 @@ impl Command for Init {
         let mut head_file = fs::File::create(HEAD_FILE)?;
         head_file.write_all(b"ref: refs/heads/main")?;
 
-        let index_file = fs::File::create(INDEX_FILE)?;
+        let _index_file = fs::File::create(INDEX_FILE)?;
         
         Ok(String::new())    
     }
@@ -255,7 +297,9 @@ impl Command for HashObject {
         }
         let content = read_file_content(path)?;
         if write {
-            return write_object_file(content, obj_type, path);
+            let file_len = get_file_length(path)?;
+            //let hash_obj_creator = HashObjectCreator::new();
+            return HashObjectCreator::write_object_file(content, obj_type, file_len);
         }
         else {
             println!("{}", generate_sha1_string(content.as_str()));
@@ -264,24 +308,34 @@ impl Command for HashObject {
     }
 }
 
-fn write_object_file(content: String, obj_type: ObjectType, path: &str) -> Result<String, Box<dyn Error>> {
-    let data = format!("{} {}\0{}", obj_type, get_file_length(path)?, content);
-    let hashed_data = generate_sha1_string(data.as_str());
-    let compressed_content = compress_content(content.as_str())?;
-    
-    let obj_directory_path = format!("{}/{}", OBJECT, &hashed_data[0..2]);
-    let _ = fs::create_dir(&obj_directory_path);
+pub struct HashObjectCreator;
 
-    let object_file_path = format!("{}/{}", obj_directory_path, &hashed_data[2..]);
-    if fs::metadata(object_file_path.clone()).is_ok() {
-        return Ok(hashed_data)
+impl HashObjectCreator {
+    fn new() -> Self {
+        HashObjectCreator {}
     }
-    
-    let mut object_file = fs::File::create(object_file_path.clone())?;
-    write!(object_file, "{:?}", compressed_content)?;    
 
-    Ok(hashed_data)
+    fn write_object_file(content: String, obj_type: ObjectType, file_len: u64) -> Result<String, Box<dyn Error>> {
+        let data = format!("{} {}\0{}", obj_type, file_len, content);
+        let hashed_data = generate_sha1_string(data.as_str());
+        let compressed_content = compress_content(content.as_str())?;
+        
+        let obj_directory_path = format!("{}/{}", OBJECT, &hashed_data[0..2]);
+        let _ = fs::create_dir(&obj_directory_path);
+    
+        let object_file_path = format!("{}/{}", obj_directory_path, &hashed_data[2..]);
+        if fs::metadata(object_file_path.clone()).is_ok() {
+            return Ok(hashed_data)
+        }
+        
+        let mut object_file = fs::File::create(object_file_path.clone())?;
+        write!(object_file, "{:?}", compressed_content)?;    
+    
+        Ok(hashed_data)
+    }
 }
+
+
 
 /// Returns lenght of the a given file's content
 fn get_file_length(path: &str) -> Result<u64, Box<dyn Error>> {
@@ -494,7 +548,7 @@ impl StagingArea {
     }
 
     fn add_file(&self, head: &mut Head, path: &str) -> Result<(), Box<dyn Error>> {
-        let mut hash_object = HashObject::new();
+        let hash_object = HashObject::new();
         let object_hash = hash_object.execute(head, Some(&["-w", path]))?;
 
         let object_path = format!("{}/{}/{}", OBJECT, &object_hash[0..2], &object_hash[2..]);
@@ -531,21 +585,21 @@ fn main() {
     //     eprintln!("{}", error);
     //     return; 
     // }
-    head.print_all();
+    //head.print_all();
 
 
     let mut add = Add::new();
-    if let Err(error) = add.execute(&mut head, Some(&["a.txt"])) {
+    if let Err(error) = add.execute(&mut head, Some(&["a/a.txt"])) {
         println!("{}", error);
         return;
     }
 
-    let mut rm = Rm::new();
-    if let Err(error) = rm.execute(&mut head, Some(&["a.txt"])) {
+    let mut commit = Commit::new();
+    if let Err(error) = commit.execute(&mut head, Some(&["-m", "message"])) {
         println!("{}", error);
         return;
     }
-    
+
     // let mut cat = CatFile::new();
     // if let Err(error) = cat.execute(&mut head, Some(&["-t", "000142551ee3ec5d88c405cc048e1d5460795102"])){
     //     eprintln!("{}", error);
