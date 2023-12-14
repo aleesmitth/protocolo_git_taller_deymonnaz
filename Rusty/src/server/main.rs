6f17b41d464ac991e4b1a6e22dc37c37eb4751a4
Alejandro Smith <smithalejandro96@gmail.com>
2023-12-14 20:11:35 -0300

    hello world api

--- Rusty/src/server/main.rs
+++ Rusty/src/server/main.rs
@@ -1,28 +1,15 @@
-use rusty::commands::git_commands::RELATIVE_PATH;
-use rusty::server::server_protocol::ServerProtocol;
-use std::env;
-use std::thread;
+#[macro_use] extern crate rocket;
 
-fn main() -> Result<(), Box<dyn std::error::Error>> {
-    env::set_var(RELATIVE_PATH, "src/server/");
-    println!("Git server is running at git://127.0.0.1:9418");
-    let listener = ServerProtocol::bind("127.0.0.1:9418")?; // Default Git port
-    println!("bind complete");
+#[get("/")]
+fn world() -> &'static str {
+    "Hello, world!"
+}
 
-    for stream in listener.incoming() {
-        match stream {
-            Ok(stream) => {
-                let mut cloned_stream = stream.try_clone()?;
-                thread::spawn(move || {
-                    if let Err(err) = ServerProtocol::handle_client_conection(&mut cloned_stream) {
-                        println!("Error: {:?}", err);
-                    }
-                });
-            }
-            Err(e) => {
-                eprintln!("Error accepting connection: {}", e);
-            }
-        }
-    }
+#[rocket::main]
+async fn main() -> Result<(), rocket::Error> {
+    let _rocket = rocket::build()
+        .mount("/", routes![world])
+        .launch()
+        .await?;
     Ok(())
 }
