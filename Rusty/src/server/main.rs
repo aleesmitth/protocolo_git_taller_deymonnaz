f79cf4509c7f52fef2e8b497a35ee9bc2e2a2097
Alejandro Smith <smithalejandro96@gmail.com>
2023-12-14 20:42:41 -0300

    server api runs on a different thread

--- Rusty/src/server/main.rs
+++ Rusty/src/server/main.rs
@@ -1,12 +1,27 @@
-#[macro_use] extern crate rocket;
+#[macro_use]
+extern crate rocket;
 
 #[get("/")]
 fn world() -> &'static str {
     "Hello, world!"
 }
 
-#[rocket::main]
-async fn main() -> Result<(), rocket::Error> {
+#[tokio::main]
+async fn main() {
+    // Spawn a new Tokio task to run the Rocket application
+    tokio::spawn(async {
+        if let Err(e) = run_rocket().await {
+            eprintln!("Rocket error: {}", e);
+        }
+    });
+
+    // Your main application logic can continue here if needed
+
+    println!("MAIN THREAD CONTINUED. SLEEP 10 AND SHUTDOWN");
+    tokio::time::sleep(std::time::Duration::from_secs(10)).await;
+}
+
+async fn run_rocket() -> Result<(), rocket::Error> {
     let _rocket = rocket::build()
         .mount("/", routes![world])
         .launch()
