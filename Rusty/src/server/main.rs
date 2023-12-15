3461729182fd6cdae31034903f9b3f0eadea5183
Alejandro Smith <smithalejandro96@gmail.com>
2023-12-14 21:42:21 -0300

    api receives param and runs git command

--- Rusty/src/server/main.rs
+++ Rusty/src/server/main.rs
@@ -1,13 +1,28 @@
 #[macro_use]
 extern crate rocket;
+use rusty::commands::git_commands::RELATIVE_PATH;
+//use rusty::server::server_protocol::ServerProtocol;
+use std::env;
+use rocket::tokio::task::spawn_blocking;
+//use std::thread;
 
-#[get("/")]
-fn world() -> &'static str {
-    "Hello, world!"
+
+use rusty::commands::git_commands;
+use rusty::commands::git_commands::Command;
+
+#[get("/repo/<repo_name>")]
+async fn world(repo_name: &str) -> String {
+    let _vec = spawn_blocking(|| {
+        if let Err(e) = git_commands::Init::new().execute(None) {
+            println!("e {}",e);
+        }
+    }).await;
+    format!("repo_name {}", repo_name)
 }
 
 #[tokio::main]
 async fn main() {
+    env::set_var(RELATIVE_PATH, "src/server/");
     // Spawn a new Tokio task to run the Rocket application
     tokio::spawn(async {
         if let Err(e) = run_rocket().await {
@@ -16,9 +31,8 @@ async fn main() {
     });
 
     // Your main application logic can continue here if needed
-
-    println!("MAIN THREAD CONTINUED. SLEEP 10 AND SHUTDOWN");
-    tokio::time::sleep(std::time::Duration::from_secs(10)).await;
+    println!("MAIN THREAD CONTINUED. SLEEP 1000 AND SHUTDOWN");
+    tokio::time::sleep(std::time::Duration::from_secs(1000)).await;
 }
 
 async fn run_rocket() -> Result<(), rocket::Error> {
